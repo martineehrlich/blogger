956cef4312def5df3b65164eab5dcf97b3ac9396
martineehrlich <martineehrlich@gmail.com>
2015-06-09 19:12:20 -0700

    added a few gems

--- app/models/article.rb
+++ app/models/article.rb
@@ -8,8 +8,11 @@ class Article < ActiveRecord::Base
   end
 
   def tag_list=(tags_string)
-  tag_names = tags_string.split(",").collect {|s| s.strip.downcase}.uniq
-  new_or_found_tags = tag_names.collect { |name| Tag.find_or_create_by(name: name) }
-  self.tags = new_or_found_tags
-end
+    tag_names = tags_string.split(",").collect {|s| s.strip.downcase}.uniq
+    new_or_found_tags = tag_names.collect { |name| Tag.find_or_create_by(name: name) }
+    self.tags = new_or_found_tags
+  end
+
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 end
